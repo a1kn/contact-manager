da0e92a0a2a1db4850658a131f4cd3ad2872fd3d
Arthur Kauffman <arthur@sent.com>
2020-06-17 17:02:32 -0400

    Initial js for events

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,41 @@
-const renderPage = {
+const app = {
   init() {
-    
+    const $initialBox = $(this.noContactsBox());
+    const $addContact = $(this.addContact('Create Contact', 'Add'));
+    $initialBox.addClass('initial');
+    $addContact.hide().addClass('add');
+    $('main').append($initialBox).fadeIn();
+    $('main').append($addContact);
+
+    this.bind();
   },
 
+  noContactsBox() {
+    const template = Handlebars.compile($('#no-contacts').html());
+    return template();
+  },
 
+  addContact(title, button) {
+    const template = Handlebars.compile($('#contact-edit').html());
+    const info = { title, button };
+    return template(info);
+  },
+
+  bind() {
+    $('a.add-contact').click(function (e) {
+      e.preventDefault();
+     
+      $('main').children().hide(function () {
+        $('div.add').show(500); 
+      });
+    });
+
+    $('button.cancel').click(function (e) {
+      $('div.add').fadeOut(function () {
+        $('div.initial').fadeIn();
+      });
+    });
+  },
 };
 
-
+app.init();
